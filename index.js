9eecef1d2d2464a29e8d75fe655b5833ea547888
Benhin <benhinmwendwa7@gmail.com>
2024-03-25 11:54:30 +0300

    function

---
+++ index.js
@@ -0,0 +1,20 @@
+//const receivesAFunction="designer"
+
+function receivesAFunction(callback){
+
+    callback("winner")
+    
+
+}callback();
+
+function returnsANamedFunction(){
+
+return  function NamedFunction(){
+}}
+console.log("winner")
+
+function returnsAnAnonymousFunction(){
+    return function()
+    {
+    console.log("winner")}
+    }
